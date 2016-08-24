2d836b7ca562203ce29d1b7643591c2082987d6a
郏李鹏 <jialipeng1991@live.com>
2016-08-24 22:46:56 +0800

    第一次修改

--- RemoteTest/RemoteConsole/Program.cs
+++ RemoteTest/RemoteConsole/Program.cs
@@ -1,4 +1,4 @@
-﻿sing System;
+﻿using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -10,6 +10,8 @@ namespace RemoteConsole
     {
         static void Main(string[] args)
         {
+            System.Console.WriteLine("Git+项目");
+            Console.ReadLine();
         }
     }
 }
