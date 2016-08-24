fe5a72676fbf8a7d08426b062e1746576c3ef4c9
郏李鹏 <jialipeng1991@live.com>
2016-08-24 22:44:33 +0800

    创建项目

---
+++ RemoteTest/RemoteConsole/Program.cs
@@ -0,0 +1,15 @@
+﻿sing System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RemoteConsole
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
